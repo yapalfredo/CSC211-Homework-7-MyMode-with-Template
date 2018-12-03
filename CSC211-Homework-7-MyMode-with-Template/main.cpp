a6c58d8500b89d8d6c8973c51d7e1db8c9118377
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-12-03 14:14:28 -0500

    MyMode with template

---
+++ CSC211-Homework-7-MyMode-with-Template/main.cpp
@@ -0,0 +1,46 @@
+#include "MyMode.h"
+
+
+int main()
+{
+	try
+	{
+		//GENERATES RANDOM NUMBERS TO BE SAVED
+		//IN THE TEXTFILE
+		fstream outFile;
+		outFile.open("RandomlyGeneratedNumbers.txt", ios::out);
+		srand(time(NULL));
+		for (size_t i = 0; i < SIZE; i++)
+		{
+			outFile << rand() % RANGE + 1 << endl;
+		}
+		outFile.close();
+		//-----------------------------------------------------------
+		//-----------------------------------------------------------
+
+		//CREATES AN fstream OBJECT
+		fstream inFile("RandomlyGeneratedNumbers.txt", ios::in);
+		MyMode<int> newMode(inFile); //pass the inFile object to the constructor
+
+		//opens another text file to save the screen output
+		outFile.open("ScreenOutput.txt", ios::out);
+		newMode.printArray(outFile);
+		outFile.close();
+		outFile.open("ScreenOutput.txt", ios::app);
+		newMode.lookForMode(outFile);
+		outFile.close();
+		cout << endl << endl;
+	}
+	catch (string str)
+	{
+		cout << str << endl;
+		exit(1);
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
+
+	system("pause");
+	return 0;
+}
